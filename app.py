001ae744ab3e5b695cbd8d2a23afa341f5a954cf
sebastian-tan605 <sebastianth-wc22@student.tarc.edu.my>
2025-05-04 22:30:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -170,7 +170,7 @@ def handle_userinput(user_question):
             grounded = max_similarity >= 0.7
 
         if not grounded or answer.lower() in ["i don't know", "i'm not sure"]:
-            answer = "I'm sorry, but I couldn't find an answer to that question in the documents you provided."
+            answer += "I'm sorry, but I couldn't find an answer to that question in the documents you provided."
         answer += f"\n\n🕒 Response time: {response_time} seconds"
         # if answer and source_docs:
         #     embedder = OpenAIEmbeddings()
